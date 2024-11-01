4b441eb8ba49b37ccfad145ac093bdd14c9c6320
Sgt_Imalas <bene.schlai@gmx.de>
2024-11-01 21:36:36 +0100

    query fix missing http

--- SeedFinder/SeedFinder.Client/Model/Search/SearchQuery.cs
+++ SeedFinder/SeedFinder.Client/Model/Search/SearchQuery.cs
@@ -4,17 +4,25 @@
     {
         public List<Dlc> ActiveDlcs = new List<Dlc>();
         public ClusterCategory? ActiveMode;
+        public ClusterLayout? SelectedCluster;
+
+
+        
 
         #region dlc
 
         public void ToggleDlc(Dlc dlc)
         {
             SetDlcEnabled(dlc, !IsDlcSelected(dlc));
-            ActiveMode = null;
         }
 
         public void SetDlcEnabled(Dlc dlc, bool enable)
         {
+
+            if (dlc.IsMainVersion)
+                ActiveMode = null;
+            if (dlc.IsMainVersion || (SelectedCluster?.RequiredDlcs.Contains(dlc)?? false) || (SelectedCluster?.ForbiddenDlcs.Contains(dlc)??false))
+                SelectedCluster = null;
 
             if (enable && !IsDlcSelected(dlc))
             {
@@ -22,6 +30,7 @@
                 {
                     DataImport.ImportGameData(dlc == Dlc.SPACEDOUT);
 
+                    ActiveMode = null;
                     if (!ActiveDlcs.Contains(dlc))
                     {
                         ActiveDlcs.RemoveAll(d => d.IsMainVersion);
@@ -29,18 +38,26 @@
                 }
                 ActiveDlcs.Add(dlc);
             }
-            else if(IsDlcSelected(dlc))
+            else if (IsDlcSelected(dlc))
             {
                 ActiveDlcs.Remove(dlc);
             }
         }
         public bool IsDlcSelected(Dlc dLC) => ActiveDlcs.Contains(dLC);
         public bool MainVersionSelected() => ActiveDlcs.Any(dlc => dlc.IsMainVersion);
-#endregion
+        #endregion
 
-        public void SelectMode(ClusterCategory category) => ActiveMode = category;
+        public void SelectMode(ClusterCategory mode)
+        {
+            ActiveMode = mode;
+            SelectedCluster = null;
+        }
         public bool IsModeSelected(ClusterCategory mode) => ActiveMode == mode;
         public bool AnyModeSelected() => ActiveMode != null;
+
+        public void SelectCluster(ClusterLayout cluster) => SelectedCluster = cluster;
+        public bool IsClusterSelected(ClusterLayout cluster) => SelectedCluster == cluster;
+        public bool AnyClusterSelected() => SelectedCluster != null;
 
     }
 }
