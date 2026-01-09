2d4b23da59fccbc7371deb0d68d42e3d03c2e65f
Sgt_Imalas <20609939+Sgt-Imalas@users.noreply.github.com>
2026-01-09 20:20:47 +0100

    idk even

--- TraitFinderApp/LocalStorageHelper.cs
+++ TraitFinderApp/LocalStorageHelper.cs
@@ -1,10 +1,36 @@
-﻿using System.Xml.Serialization;
+﻿using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace TraitFinderApp
 {
     public static class LocalStorageHelper
     {
-        public const string HideTraitlessKey = "MNI_Traitfinder_HideTraitless";
+		public static async ValueTask<T?> GetItemAsyncWithConsent<T>(this Blazored.LocalStorage.ILocalStorageService service, string key)
+		{
+			if (!LocalStorageConsentGiven)
+				return await new ValueTask<T?>(default(T));
+
+			return await service.GetItemAsync<T>(key);
+		}
+		public static async Task SetItemAsyncWithConsent<T>(this Blazored.LocalStorage.ILocalStorageService service, string key, T value)
+		{
+			if (!LocalStorageConsentGiven)
+				return;
+
+			await service.SetItemAsync<T>(key,value);
+		}
+
+		public static async Task FetchConsent(this Blazored.LocalStorage.ILocalStorageService service)
+		{
+			LocalStorageConsentGiven = await service.GetItemAsync<bool>(LocalStorageConsentGivenKey);
+		}
+
+		public static async Task SetConsentGiven(this Blazored.LocalStorage.ILocalStorageService service) => await service.SetItemAsync<bool>(LocalStorageConsentGivenKey, true);
+
+		public const string LocalStorageConsentGivenKey = "MNI_LocalStorageConsent";
+		public static bool LocalStorageConsentGiven = false;
+
+		public const string HideTraitlessKey = "MNI_Traitfinder_HideTraitless";
         public const string PersistantTraitOrderingKey = "MNI_Traitfinder_OrderTraitsPersistant";
         public const string HideLocationlessDistancesKey = "MNI_Traitfinder_HideLocationlessStarmapBands";
         public const string MaxSeedNumberPerSearchKey = "MNI_Traitfinder_MaxSeedNumberPerSearch";
@@ -12,6 +38,9 @@ namespace TraitFinderApp
 		public const string SavedMixingsKey = "MNI_Traitfinder_SavedMixings";
 		public const string SavedSettingsKey = "MNI_Traitfinder_SavedSettings";
 		public const string SavedStoryTraitsKey = "MNI_Traitfinder_SavedStoryTraits";
+
+
+
 
 		public static bool DarkThemeActive = true;
         public static bool UsePersistentTraitOrdering = false;
@@ -22,7 +51,9 @@ namespace TraitFinderApp
 		public static string EmbbeddedIn = null;
         public static string MNI_Token = null;
 
-        public static bool TryGetMNIToken(out string token)
+
+
+		public static bool TryGetMNIToken(out string token)
 		{
 			token = MNI_Token;
 			return !string.IsNullOrEmpty(token);
